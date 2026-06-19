ee168fd6c8f6f72395476ad883ac06e761ff1417
Dr. Agnaldo Ferreira <agnaldo@example.com>
2026-06-19 16:59:13 -0300

    fix: explicit connection to default database instance ID

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,7 +5,7 @@ import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db = getFirestore(app, 'default');
 
 const provider = new GoogleAuthProvider();
 provider.addScope('https://www.googleapis.com/auth/drive.file');
